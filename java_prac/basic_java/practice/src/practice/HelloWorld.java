83049c0350dc031a8c54c9f3711bc41253859871
marsa <m4ppom@gmail.com>
2020-01-24 18:47:04 +0900

    java

---
+++ java_prac/basic_java/practice/src/practice/HelloWorld.java
@@ -0,0 +1,41 @@
+package practice;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("주석하는법?");
+		int marsa;
+		final int MARSA2 = 2;
+		final double PI = 3.1415926535;
+		
+		double circleArea;
+		circleArea = 3*3*PI;
+		
+		int i1 = 5;
+		int i2 = 3;
+		int i3 = 4;
+		
+		int i4 = ++i3;
+		System.out.println(i4);
+		
+		int i5 = i3++;
+		
+		System.out.println(i5);
+		System.out.println(i3);
+		
+		
+		int ii = 10;
+		int jj = 10;
+		System.out.println(ii == jj);
+		
+		if (i1 > i2) {
+			System.out.println("li is bigger than l2");
+		}
+		else {
+			
+		}
+	}
+
+}
